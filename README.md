73b9bb5ad5cb28b17ed3794564b94ba0bd0a18cd
Matheus Alvarenga <matheus@hubway.com.br>
2021-07-09 11:59:10 -0300

    Finished project

--- README.md
+++ README.md
@@ -1,27 +1,13 @@
-# PasswordGenerator
+# Password Generator
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.1.1.
+### Um projeto feito com o curso: _"The Modern Angular Bootcamp"_
 
-## Development server
+Link do Curso: https://www.udemy.com/course/the-modern-angular-bootcamp
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+### Tecnologias utilizadas no projeto:
 
-## Code scaffolding
+1. Angular
+2. ESLint
+3. Bulma
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+### Link para o Live Preview: <br> https://matheusalvarenga.github.io/system_password_gen_angular/
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'password-generator';
+    length = 0;
+
+    password = '';
+
+    useLetters = false;
+
+    useNumbers = false;
+
+    useSymbols = false;
+
+    handleButtonClick() {
+        const teste = '123';
+        const letters = 'abcdefghijklmnopqrstuvwxyz';
+        const symbols = '!@#$%*()_+-./[]={}?><';
+
+        let validChars = '';
+
+        if (this.useLetters) {
+            validChars += letters;
+        }
+
+        if (this.useNumbers) {
+            validChars += teste;
+        }
+
+        if (this.useSymbols) {
+            validChars += symbols;
+        }
+
+        let generatedPassword = '';
+
+        for (let i = 0; i < this.length; i += 1) {
+            const index = Math.floor(Math.random() * validChars.length);
+
+            generatedPassword += validChars[index];
+        }
+
+        this.password = generatedPassword;
+    }
+
+    handleLength(text: string) {
+        const parsed = parseInt(text, 10);
+
+        // eslint-disable-next-line no-restricted-globals
+        if (!isNaN(parsed)) {
+            this.length = parsed;
+        }
+    }
+
+    handleLetters() {
+        this.useLetters = !this.useLetters;
+    }
+
+    handleNumbers() {
+        this.useNumbers = !this.useNumbers;
+    }
+
+    handleSymbols() {
+        this.useSymbols = !this.useSymbols;
+    }
 }
